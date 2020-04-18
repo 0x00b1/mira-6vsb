8998a5f14b8f607246deec08f9ded60657314ced
Allen Goodman <allen.goodman@icloud.com>
2020-04-18 14:41:13 -0400

    cleanup

--- 6vsb.py
+++ 6vsb.py
@@ -1,11 +1,32 @@
-import collections
-import itertools
 import os
-import random
-import urllib.request
 
 import pyknon.genmidi
 import pyknon.music
+
+AMINO_ACIDS = {
+    "A": 0,
+    "R": 1,
+    "N": 2,
+    "D": 3,
+    "B": 4,
+    "C": 5,
+    "Q": 6,
+    "E": 7,
+    "Z": 8,
+    "G": 9,
+    "H": 10,
+    "I": 11,
+    "L": 12,
+    "K": 13,
+    "M": 14,
+    "F": 15,
+    "P": 16,
+    "S": 17,
+    "T": 18,
+    "W": 19,
+    "Y": 20,
+    "V": 21
+}
 
 if __name__ == "__main__":
     with open("./data/rcsb_pdb_6VSB.fasta") as fp:
@@ -13,34 +34,9 @@ if __name__ == "__main__":
 
     sequence = sequence.splitlines()[-1].strip()
 
-    amino_acids = {
-        "A": 0,
-        "R": 1,
-        "N": 2,
-        "D": 3,
-        "B": 4,
-        "C": 5,
-        "Q": 6,
-        "E": 7,
-        "Z": 8,
-        "G": 9,
-        "H": 10,
-        "I": 11,
-        "L": 12,
-        "K": 13,
-        "M": 14,
-        "F": 15,
-        "P": 16,
-        "S": 17,
-        "T": 18,
-        "W": 19,
-        "Y": 20,
-        "V": 21
-    }
-
     notes = []
 
-    for note in [amino_acids[amino_acid] for amino_acid in sequence]:
+    for note in [AMINO_ACIDS[amino_acid] for amino_acid in sequence]:
         notes += [pyknon.music.Note(note)]
 
     midi = pyknon.genmidi.Midi()
